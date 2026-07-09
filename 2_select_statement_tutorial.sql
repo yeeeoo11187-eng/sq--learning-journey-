947ce0c55d14c4efce2b691454f23a2620c0a4da
yeeeoo11187-eng <yeeeoo11187@gmail.com>
2026-07-09 21:40:58 +0800

    Create 2_select_statement_tutorial.sql
    2_select_statement_tutorial

---
+++ 2_select_statement_tutorial.sql
@@ -0,0 +1,12 @@
+select first_name,
+last_name,
+age,
+age+(10+10)*10 
+From parks_and_recreation.employee_demographics;
+# PEMDAS
+
+select distinct gender
+From parks_and_recreation.employee_demographics;
+
+select distinct first_name,gender 
+From parks_and_recreation.employee_demographics;
